829e5069e8ac4940d4544d0542ff11f7c0b72ad5
ReidChenJX <327817014@qq.com>
2021-03-30 14:01:12 +0800

    New fires

---
+++ structure/linknode.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# @time     : 2021/3/10 17:07
+# @Author   : ReidChen
+# Document  ：
+
+
+class Node:
+    
+    pass
+
+class Tree:
+    
+    pass
---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# @time     : 2021/3/10 17:26
+# @Author   : ReidChen
+# Document  ：
+
+from structure import linknode
+
